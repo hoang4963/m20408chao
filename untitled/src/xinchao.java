dda42a6e1c1277d60928ecb928b020fa32cb999c
hoang4963 <hoangth.finbox@gmail.com>
2022-08-04 10:34:25 +0700

    a

---
+++ untitled/src/xinchao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class xinchao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Ten ban la gi??");
+        String name = scanner.nextLine();
+        System.out.println(" Xin chao " + name);
+    }
+
+}
